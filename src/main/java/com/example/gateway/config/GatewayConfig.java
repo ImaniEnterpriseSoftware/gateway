c272d21cd0d4c7cf9d34bb8bc0391cce0521b803
ImaniLeemans <455181@student.fontys.nl>
2023-06-18 14:02:16 +0200

    gateway config changes

--- src/main/java/com/example/gateway/config/GatewayConfig.java
+++ src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -11,11 +11,22 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("backend", r -> r.path("/api/**")
+                .route("backend", r -> r.path("/api/description/**")
                         .uri("http://description-cud:8081"))
-                .route("backend", r -> r.path("/api/**")
+                .route("backend", r -> r.path("/api/descriptions/**")
                         .uri("http://description-r:8082"))
+                .route("backend", r -> r.path("/api/users/**")
+                        .uri("http://user-service:8083"))
                 .build();
+
+//        return builder.routes()
+//                .route("backend", r -> r.path("/api/description/**")
+//                        .uri("http://localhost:8081"))
+//                .route("backend", r -> r.path("/api/descriptions/**")
+//                        .uri("http://localhost:8082"))
+//                .route("backend", r -> r.path("/api/users/**")
+//                        .uri("http://localhost:8083"))
+//                .build();
 
     }
 }
